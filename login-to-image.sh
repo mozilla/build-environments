c6c93654d5ed38f106412e99327f7b049136fe83
Morgan Phillips <winter2718@gmail.com>
2015-02-07 19:47:17 +0000

    Make give all image logins a unique name, based on a timestamp

--- login-to-image.sh
+++ login-to-image.sh
@@ -5,4 +5,4 @@
 if [ ! $# -gt 1 ]; then
     echo "Usage: login-to-image.sh <image_name>"
 fi
-docker run -i -t --name ${2-login_to_image} $1 /bin/bash
+docker run -i -t --name login_to_image_$(date +%s) $1 /bin/bash
