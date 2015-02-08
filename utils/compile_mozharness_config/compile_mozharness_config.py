210a75ba16599842b3c8d73cafe113491dd39fe3
Morgan Phillips <winter2718@gmail.com>
2015-02-08 21:43:50 +0000

    Add license header

--- utils/compile_mozharness_config/compile_mozharness_config.py
+++ utils/compile_mozharness_config/compile_mozharness_config.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+# ***** END LICENSE BLOCK *****
+
+'''
+Compile jinja2 templates with variables passed in from a mozharness config.
+'''
 
 import jinja2
 from mozharness.base.config import parse_config_file
