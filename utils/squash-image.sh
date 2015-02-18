fcdcbc1d9cb2fa0ac63071420c996c4c617e6604
Morgan Phillips <winter2718@gmail.com>
2015-02-18 15:43:20 +0000

    Remove dependency on docker-squash.

--- utils/squash-image.sh
+++ utils/squash-image.sh
@@ -2,16 +2,11 @@
 
 set -eux
 
-# do all of the docker squash steps.
-# REQUIRES: docker-squash: https://github.com/jwilder/docker-squash
+# flatten out an image!
 
 if [ ! $# -gt 1 ]; then
-    echo "Usage: ./squash-image.sh <image> [docker-squash options| ...]"
+    echo "Usage: ./squash-image.sh <image> <tag_name>"
     exit
 fi
-
-docker save $1 > image.tar
-sudo docker-squash -i image.tar -o squashed.tar ${@:2}
-cat squashed.tar | docker load
-rm image.tar
-rm squashed.tar
+ID=$(docker run -d $1 /bin/bash)
+docker export $ID | docker import - $2
